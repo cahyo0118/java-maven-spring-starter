b9cf372f26a6577dbae30687fe0eef46c112a77f
N. Cahyo <nurcahyoputro0@gmail.com>
2020-12-21 22:34:33 +0700

    add user end-point

---
+++ src/main/java/com/dicicip/starter/config/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.dicicip.starter.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests().anyRequest().permitAll();
+
+    }
+
+}
---
+++ src/main/java/com/dicicip/starter/util/file/FileUtil.java
@@ -0,0 +1,11 @@
+package com.dicicip.starter.util.file;
+
+public class FileUtil {
+
+    private String rootFilesDirectory;
+    private String relativeFilesDirectory;
+    private int directoryPermission;
+
+
+
+}
